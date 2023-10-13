49897934bd44d4d0ced334d264768f8f770a68fc
sajib-rana <sajibrana444@gmail.com>
2023-10-13 12:58:02 +0600

    create token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -27,7 +28,19 @@ async function run() {
     await client.connect();
      const serviceCollection = client.db("computer-mechanics").collection("services");
      const bookingCollection = client.db("computer-mechanics").collection("bookings");
+       
+    //  jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      console.log(token);
+      res.send({ token });
+    });
 
+        // servcie related API
       app.get("/services", async (req, res) => {
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
